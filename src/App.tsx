0d4e83659bac943950ccecd6934e1458182a990f
iamamritpalrandhawa <amritpalsinghrandhawa76@gmail.com>
2025-04-05 17:23:35 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+// import Navbar from "./components/Nabvar";
+import HomePage from "./pages/HomePage";
+// import BusesPage from "./pages/BusPage";
+// import { Card, CardContent } from "@/components/ui/card";
+// import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+// import { Table, TableBody, TableHead, TableRow, TableCell } from "@/components/ui/table";
+// import { Bus, Route, User } from "lucide-react";
+// import { Marquee } from "@devnomic/marquee";
+
+// import LoginPage from "./pages/LoginPage";
+
+function App() {
+  // const actions = [
+  //   { title: "Find My Bus", icon: <Bus className="h-6 w-6" /> },
+  //   { title: "Routes", icon: <Route className="h-6 w-6" /> },
+  //   { title: "Profile", icon: <User className="h-6 w-6" /> },
+  // ];
+  return (
+    <>
+      <HomePage />
+    </>
+    //   <div className="w-full max-w-sm">
+    //     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    //   </div>
+    // </div>
+  );
+}
+
+export default App;
